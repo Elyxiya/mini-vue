4451c1773178f866bf9ead65e67455ffa1ac4ca0
Elyxiya <3013693518@qq.com>
2025-07-07 09:53:21 +0800

    feat: readonly功能

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,26 +1,50 @@
+import { mutableHandlers, readonlyHandlers } from "./baseHandler";
 import { track, trigger } from "./effect";
 
+
 export function reactive(raw) {
+  return createActiveObject(raw,mutableHandlers);
+  
+  // return new Proxy(raw, {
     
+  //   // get(target, key) {
+  //   //     // {foo: 1}
+  //   //     // foo
+  //   //     const res = Reflect.get(target, key);
 
-  return new Proxy(raw, {
-    
-    get(target, key) {
-        // {foo: 1}
-        // foo
-        const res = Reflect.get(target, key);
-
-        // TODO 依赖收集
-        track(target, key);
-        return res;
-    },
-
-    set(target, key, value) {
-      const res = Reflect.set(target, key, value);
+  //   //     // TODO 依赖收集
+  //   //     track(target, key);
+  //   //     return res;
+  //   // },
+  //   get: createGetter(),
+  //   // set(target, key, value) {
+  //   //   const res = Reflect.set(target, key, value);
       
-      // TODO 触发更新
-      trigger(target, key)
-      return res;
-    },
-  });
+  //   //   // TODO 触发更新
+  //   //   trigger(target, key)
+  //   //   return res;
+  //   // },
+  //   set: createSetter(),
+  // });
+}
+
+export function readonly(raw){
+  return createActiveObject(raw,readonlyHandlers);
+
+  // return new Proxy(raw, { 
+  //   // get(target, key) {
+  //   //   const res = Reflect.get(target, key);
+
+  //   //   return res;
+  //   // },
+  //   get: createGetter(true),
+  //   set(target, key, value) {
+  //     // console.warn(`key: ${key} set failed, because target is readonly`);
+  //     return true;
+  //   }
+  // })
+}
+
+function createActiveObject(raw:any, baseHandler) {
+  return new Proxy(raw, baseHandler);
 }
---
+++ src/reactivity/tests/readonly.spec.ts
@@ -0,0 +1,25 @@
+import { readonly } from "../reactive";
+describe("readonly",() => {
+  it("happy path",() => {
+
+    //not set
+    const original = {foo:1,bar:{ baz:2 }};
+    const warpped = readonly(original);
+    expect(warpped).not.toBe(original);
+    expect(warpped.foo).toBe(1);
+  });
+
+  it('warn then call set',() => {
+    //console.warn()
+    //mock
+    console.warn = jest.fn();
+
+    const user = readonly({
+      age:10
+    })
+
+    user.age = 11;
+
+    expect(console.warn).toHaveBeenCalled ();
+  })
+})
