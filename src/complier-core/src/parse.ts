c0e6acc422aa9b84a890905fa5f3d872ab1717cc
Elyxiya <3013693518@qq.com>
2025-07-27 22:39:18 +0800

    feat: 实现解析element标签

--- src/complier-core/src/parse.ts
+++ src/complier-core/src/parse.ts
@@ -1,5 +1,10 @@
 import { NodeTypes } from "./ast";
 
+
+const enum TagType {
+  Start,
+  End,
+}
 export function baseParse (content: string) {
    const context = createParserContext(content);
 
@@ -9,8 +14,13 @@ export function baseParse (content: string) {
 function parseChildren(context) {
   const nodes: any = [];
   let node;
-  if(context.source.startsWith("{{")) {
+  const s = context.source;
+  if(s.startsWith("{{")) {
      node = parseInterpolation(context);
+  } else if(s[0] === "<") {
+    if(/[a-z]/i.test(s[1])){
+      node = parseElement(context);
+    }
   }
  
   
@@ -19,7 +29,29 @@ function parseChildren(context) {
    return nodes;
 };
 
-function parseInterpolation(context) {
+function parseElement(context: any) { 
+   const element = parseTag(context, TagType.Start);
+
+   parseTag(context, TagType.End)
+
+   return element;
+}
+function parseTag(context: any, type: TagType) { 
+   // 1. 解析tag
+   const match: any = /<\/?([a-z]*)/i.exec(context.source);
+   const  tag = match[1];
+   // 2. 删除处理完成的代码
+   advanceBy(context, match[0].length);
+   advanceBy(context, 1);
+
+   if(type === TagType.End) return;
+   return {
+     type: NodeTypes.ElEMENT,
+     tag,
+   }
+}
+
+function parseInterpolation(context: any) {
     // {{message}}
     const openDelimiter = '{{';
     const closeDelimiter = '}}';
--- src/complier-core/test/parse.spec.ts
+++ src/complier-core/test/parse.spec.ts
@@ -2,7 +2,7 @@ import { NodeTypes } from '../src/ast';
 import { baseParse } from '../src/parse'
 
 describe('Parse', () => {
-
+  // 解析插值
   describe('interpolation',() => {
     test('simple interpolation', () => {
       const ast = baseParse("{{ message }}");
@@ -16,6 +16,17 @@ describe('Parse', () => {
         }
       })
     })
-   
-  })
+  });
+  // 解析element
+  describe('element',() => { 
+    it('simple element div', () => { 
+      const ast = baseParse("<div></div>");
+
+      // root
+      expect(ast.children[0]).toStrictEqual({
+        type: NodeTypes.ElEMENT,
+        tag: "div",
+      })
+    })
+  });
 })
